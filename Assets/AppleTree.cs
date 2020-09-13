904337ddd4ff4896ecc83676743c3e87134f631c
Mohamed Abdelwadood <mabdelw@siue.edu>
2020-09-13 15:21:18 -0500

    AppleTree
    Finished apple tree

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
     // Start is called before the first frame update
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondBetweenAppleDrops = 1f;
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondBetweenAppleDrops);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if (pos.x < -leftAndRightEdge )
+        {
+            speed = Mathf.Abs(speed);
+
+        } else if ( pos.x> leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        else if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+    void FixdUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
